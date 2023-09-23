be5caed8f2857b605d07ec18888e1a783eb53cdd
ZackeryRSmith <zackery.smith82307@gmail.com>
2023-09-23 17:04:49 -0400

    global header
    this has the same function as header.hpp

---
+++ include/global.hpp
@@ -0,0 +1,25 @@
+#ifndef BONGO_CAT_GLOBAL_HPP
+#define BONGO_CAT_GLOBAL_HPP
+extern "C" {
+#include "lauxlib.h"
+#include "lua.h"
+#include "lualib.h"
+}
+
+#include <LuaBridge.h>
+#include <SFML/Graphics.hpp>
+
+// types
+using UIntRef = const unsigned int &;
+using UInt32Ref = const uint32_t &;
+using Vec2iRef = const sf::Vector2i &;
+
+#define MAX_FRAMERATE 60
+#define BASE_WIDTH 612
+#define BASE_HEIGHT 354
+
+// the _mainWindow and _LuaState symbol are defined in main.cpp
+extern sf::RenderWindow mainWindow;
+extern lua_State *LuaState;
+
+#endif
