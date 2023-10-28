31aecf0d43f4e8bf13ce2161b3f65416c5772cec
ZackeryRSmith <zackery.smith82307@gmail.com>
2023-10-28 07:04:12 -0400

    BongoSprite impl.

---
+++ src/sprite.cpp
@@ -0,0 +1,37 @@
+#include <global.hpp>
+#include <sprite.hpp>
+
+float max_sprite_width = 0.0;
+float max_sprite_height = 0.0;
+
+//============================================================================
+// LOAD FROM ...
+//============================================================================
+sf::Sprite *BongoSprite::loadFromFile(const std::string &filename) {
+    sf::Texture *texture = new sf::Texture();
+    texture->loadFromFile(filename);
+
+    sf::Sprite *sprite = new sf::Sprite();
+    sprite->setTexture(*texture);
+
+    if (sprite->getLocalBounds().width > max_sprite_width)
+        max_sprite_width = sprite->getLocalBounds().width;
+    if (sprite->getLocalBounds().height > max_sprite_height)
+        max_sprite_height = sprite->getLocalBounds().height;
+
+    std::cout << "max sw: " << max_sprite_width
+              << "  max sh: " << max_sprite_height << std::endl;
+
+    return sprite;
+}
+
+//============================================================================
+// BIND TO LUA
+//============================================================================
+void BongoSprite::bindToLua() {
+    luabridge::getGlobalNamespace(LuaState)
+        .beginNamespace("BongoSprite")
+        // load functions
+        .addFunction("loadFromFile", &BongoSprite::loadFromFile)
+        .endNamespace();
+}
