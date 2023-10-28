4eef82d91054ba536d29b600bcede6708ec93866
ZackeryRSmith <zackery.smith82307@gmail.com>
2023-10-28 07:09:10 -0400

    Simple test of displaying a sprite

--- src/test.lua
+++ src/test.lua
@@ -2,26 +2,24 @@
 -- Just testing lua support --
 --==========================--
 
+-- laad our images. LOAD BEFORE CREATING THE WINDOW!
+local up = BongoSprite.loadFromFile("cats/osu/up.png")
+local left = BongoSprite.loadFromFile("cats/osu/left.png")
+local right = BongoSprite.loadFromFile("cats/osu/right.png")
+local wave = BongoSprite.loadFromFile("cats/osu/wave.png")
+local smoke = BongoSprite.loadFromFile("cats/osu/smoke.png")
+local mousebg = BongoSprite.loadFromFile("cats/osu/mousebg.png")
+local testbg = BongoSprite.loadFromFile("cats/osu/temp.png")
+
 -- create a window
-BongoWindow.create(800, 450, Sfml.Style.Default)
-
--- laad an image into a texture
-local texture = Sfml.Texture()
-if not texture:loadFromFile("res/catbanner.jpg", Sfml.IntRect()) then
-	print("Error loading image")
-	return
-end
-
--- create a sprite and set it's texture
-local sprite = Sfml.Sprite()
-sprite:setTexture(texture)
+BongoWindow.create(612, 354)
 
 while BongoWindow.processEvents() == 0 do
 	-- clear the window with a color (optional but recommended)
-	BongoWindow.clear(Sfml.Color.Black)
+	BongoWindow.clear(Sfml.Color.White)
 
 	-- draw the sprite to the window
-	BongoWindow.draw(sprite)
+	BongoWindow.draw(mousebg)
 
 	-- display the window's contents
 	BongoWindow.display()
