48b837e35ba8ba5d688079d4893dd50cfd157dea
ZackeryRSmith <zackery.smith82307@gmail.com>
2024-01-11 06:08:38 -0500

    drawifelse testing

--- src/test.lua
+++ src/test.lua
@@ -3,50 +3,34 @@
 --==========================--
 
 -- laad our images. LOAD BEFORE CREATING THE WINDOW!
-local up = BongoSprite.loadFromFile("cats/osu/up.png")
-local left = BongoSprite.loadFromFile("cats/osu/left.png")
-local right = BongoSprite.loadFromFile("cats/osu/right.png")
-local wave = BongoSprite.loadFromFile("cats/osu/wave.png")
-local smoke = BongoSprite.loadFromFile("cats/osu/smoke.png")
-local mousebg = BongoSprite.loadFromFile("cats/osu/mousebg.png")
-local testbg = BongoSprite.loadFromFile("cats/osu/temp.png")
+local cat = BongoSprite.loadFromFile("cats/original/cat.png")
+local mouth_shut = BongoSprite.loadFromFile("cats/original/mouth_shut.png")
+local mouth_open = BongoSprite.loadFromFile("cats/original/mouth_open.png")
+local left_down = BongoSprite.loadFromFile("cats/original/left_down.png")
+local left_up = BongoSprite.loadFromFile("cats/original/left_up.png")
+local right_down = BongoSprite.loadFromFile("cats/original/right_down.png")
+local right_up = BongoSprite.loadFromFile("cats/original/right_up.png")
 
 -- states
-local up_state = false
 local left_state = false
 local right_state = false
-local wave_state = false
-local smoke_state = false
 
--- create a window
-BongoWindow.create(612, 354)
+BongoWindow.create(800, 450)
 
 while BongoWindow.processEvents() == 0 do
-	-- clear the window with a color (optional but recommended)
 	BongoWindow.clear(Sfml.Color.White)
 
 	-- state logic
-	left_state = isPressed(BongoInput.Key.z) and not right_state
-	right_state = isPressed(BongoInput.Key.x) and not left_state
-	wave_state = isPressed(BongoInput.Key.w)
-	smoke_state = ternary(onPressed(BongoInput.Key.s), not smoke_state, smoke_state)
-	up_state = not (left_state or right_state or wave_state)
+	left_state = isPressed("z")
+	right_state = isPressed("x")
 
 	-- draw the bg sprite to the window
-	BongoWindow.draw(mousebg)
+	BongoWindow.draw(cat)
 
-	-- draw if logic
-	BongoWindow.drawif(left, left_state)
-	BongoWindow.drawif(right, right_state)
-	BongoWindow.drawif(wave, wave_state)
-	BongoWindow.drawif(smoke, smoke_state)
-	BongoWindow.drawif(up, up_state)
-
-	-- mouse hand logic
-	local mdata = BongoInput.Mouse.positionOnHoveredWindow()
-	local x, y = mdata[1], mdata[2]
-
-	bongoDebug("(" .. x .. ", " .. y .. ")")
+	-- draw ifelse logic
+	BongoWindow.drawifelse(mouth_open, left_state and right_state, mouth_shut)
+	BongoWindow.drawifelse(left_down, left_state, left_up)
+	BongoWindow.drawifelse(right_down, right_state, right_up)
 
 	-- display the window's contents
 	BongoWindow.display()
