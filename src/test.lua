04f012dfc8c5fbc6c16e8df36679baf373d49dff
ZackeryRSmith <zackery.smith82307@gmail.com>
2024-01-13 17:04:11 -0500

    hardworking cat!
    sound was tested, hense the `click.mp3` file found under `cats/hard_worker`. As I was very unhappy with the implementation of BongoAudio the tests have been excluded from this commit

--- src/test.lua
+++ src/test.lua
@@ -3,32 +3,33 @@
 --==========================--
 
 -- laad our images. LOAD BEFORE CREATING THE WINDOW!
-local cat = BongoSprite.loadFromFile("cats/original/cat.png")
-local mouth_shut = BongoSprite.loadFromFile("cats/original/mouth_shut.png")
-local mouth_open = BongoSprite.loadFromFile("cats/original/mouth_open.png")
-local left_down = BongoSprite.loadFromFile("cats/original/left_down.png")
-local left_up = BongoSprite.loadFromFile("cats/original/left_up.png")
-local right_down = BongoSprite.loadFromFile("cats/original/right_down.png")
-local right_up = BongoSprite.loadFromFile("cats/original/right_up.png")
+local cat = BongoSprite.loadFromFile("cats/hard_worker/cat.png")
+local eyes1 = BongoSprite.loadFromFile("cats/hard_worker/eyes1.png")
+local eyes2 = BongoSprite.loadFromFile("cats/hard_worker/eyes2.png")
+local left_down = BongoSprite.loadFromFile("cats/hard_worker/left_down.png")
+local left_up = BongoSprite.loadFromFile("cats/hard_worker/left_up.png")
+local right_down = BongoSprite.loadFromFile("cats/hard_worker/right_down.png")
+local right_up = BongoSprite.loadFromFile("cats/hard_worker/right_up.png")
 
 -- states
 local left_state = false
 local right_state = false
 
-BongoWindow.create(800, 450)
+BongoWindow.create()
 
 while BongoWindow.processEvents() == 0 do
 	BongoWindow.clear(Sfml.Color.White)
 
 	-- state logic
+	-- (I want to have it so one half of the keyboard does left and the other does right)
 	left_state = isPressed("z")
 	right_state = isPressed("x")
 
 	-- draw the bg sprite to the window
 	BongoWindow.draw(cat)
+	BongoWindow.draw(eyes1)
 
 	-- draw ifelse logic
-	BongoWindow.drawifelse(mouth_open, left_state and right_state, mouth_shut)
 	BongoWindow.drawifelse(left_down, left_state, left_up)
 	BongoWindow.drawifelse(right_down, right_state, right_up)
 
